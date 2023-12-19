3286d2ad540c65d43fa8c4c7f0fa618e728c0fa4
Adeyomola <kayzeel@yahoo.com>
2023-12-19 02:14:39 +0100

    added wsgi.py

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import os
+import tempfile
+import pytest
+from sentio.app import create_app
+from sentio.db import get_db, init_db
+import sqlalchemy as db
+
+
+db_password=os.environ.get('DB_PASSWORD')
+db_user=os.environ.get('DB_USER')
+host=os.environ.get('HOST')
+db_name='test'
+
+@pytest.fixture
+def app():
+    app = create_app({'TESTING': True})
+    engine = get_db(engine=db.create_engine(f"mysql://{db_user}:{db_password}@{host}/{db_name}"))
