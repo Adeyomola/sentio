18ba663c2f2b137d5e97b2652a958488e04303c6
Adeyomola <kayzeel@yahoo.com>
2023-12-19 02:11:45 +0100

    working on docker-compose and tests

--- sentio/app.py
+++ sentio/app.py
@@ -1,34 +1,33 @@
 from flask import Flask, render_template, session, redirect
 
-app = Flask(__name__)
-app.config.from_mapping(
-    SECRET_KEY='dev'
-)
 
-from sentio.blog import blog
-# from blog import blog
-app.register_blueprint(blog.bp)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='dev'
+    )
 
-from sentio.auth import auth
-# from auth import auth
-app.register_blueprint(auth.bp)
+    from sentio.blog import blog
+    app.register_blueprint(blog.bp)
 
-from sentio.blog.blog import author_posts
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    if not session:
-        return render_template('index.html')
-    elif 'firstname' in session:
-        firstname = session['firstname']
+    from sentio.auth import auth
+    app.register_blueprint(auth.bp)
+
+    from sentio.blog.blog import author_posts
+    @app.route('/', methods=['GET', 'POST'])
+    def home():
+        if not session:
+            return render_template('index.html')
+        elif 'firstname' in session:
+            firstname = session['firstname']
         return render_template('home.html', firstname=firstname, posts=author_posts())
 
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect('/login')
+    @app.route('/logout')
+    def logout():
+        session.clear()
+        return redirect('/login')
 
-import sentio.db as db
-db.init_app(app)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    import sentio.db as db
+    db.init_app(app)
+    return app
+app = create_app()
