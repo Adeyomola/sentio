a30e0d1bda1b17530e2d9dc6a0728bcccd716e6e
Adeyomola <kayzeel@yahoo.com>
2023-12-19 03:24:27 +0100

    containerization

---
+++ scripts/conf_editor.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+touch .env
+
+echo "export DB_PASSWORD=$DB_PASSWORD" >> .env
+echo "export DB_USER=$DB_USER" >> .env
+echo "export DATABASE=$DATABASE" >> .env
+echo "export HOST=$HOST" >> .env
