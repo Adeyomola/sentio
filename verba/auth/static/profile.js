bdd74f819e62700a807e18bbed420f7293810e5c
Adeyomola <kayzeel@yahoo.com>
2024-01-17 05:33:04 +0100

    major redesign

--- verba/auth/static/profile.js
+++ verba/auth/static/profile.js
@@ -11,11 +11,15 @@ const elements = elementStrings.map((element) => {
 });
 
 elements[0].addEventListener("click", () => {
+  elements[0].style.color = "lightskyblue";
+  elements[1].style.color = "";
   elements[2].style.display = "flex";
   elements[3].style.display = "none";
 });
 
 elements[1].addEventListener("click", () => {
-  elements[3].style.display = "flex";
+  elements[0].style.color = "#1d242c";
   elements[2].style.display = "none";
+  elements[1].style.color = "lightskyblue";
+  elements[3].style.display = "flex";
 });
