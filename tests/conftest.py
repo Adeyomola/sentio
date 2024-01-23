2c5c47aabb6ecffcd4999b89bafc1326acebc3b5
Adeyomola <kayzeel@yahoo.com>
2024-01-23 20:18:17 +0100

    testing out unit testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,23 @@
 import os
+import tempfile
 import pytest
 from verba.app import create_app
 from verba.db import get_db, init_db
 from verba.metadata import metadata
-from sqlalchemy import insert
-import sqlalchemy
+from sqlalchemy import insert, create_engine
+import time
 
 @pytest.fixture
 def app():
+    db_fd, db_path = tempfile.mkstemp(dir=os.getcwd())
+
     app = create_app({
         'TESTING': True,
-        'ENGINE':  sqlalchemy.create_engine("sqlite:///test"),
+        'ENGINE':  create_engine(f"sqlite:///{db_path}"),
     })
     md = metadata()
     users = md.tables['users']
     post = md.tables['post']
-
 
     create_user = (insert(users).values(username='test', password='pbkdf2:sha256:50000$TCI4GzcX$0de171a4f4dac32e3364c7ddc7c14f3e2fa61f2d17574483f7ffbb431b4acb2f', firstname='test', lastname='tester', email='test@test.com'))
     create_user2 = (insert(users).values(username='other', password='pbkdf2:sha256:50000$kJPKsz6N$d2d4784f1b030a9761f5ccaeeaca413f27f2ecb76d6168407af962ddce849f79', firstname='jon', lastname='doe', email='jonathan@lo.com'))
@@ -27,8 +29,10 @@ def app():
         get_db().execute(create_user2)
         get_db().execute(insert_post)
     yield app
+    os.close(db_fd)
+    os.unlink(db_path)
 
-    os.remove('test')
+    
 
 
 @pytest.fixture
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,15 +1,14 @@
 import pytest
 from verba.db import get_db, init_db, close_db
 from verba.metadata import metadata
-from sqlalchemy import select
-from sqlalchemy.exc import ResourceClosedError
+from sqlalchemy import select, exc
 
 
 def test_get_db(app):
     with app.app_context():
         db = get_db()
         assert db is get_db()
-    with  pytest.raises(ResourceClosedError) as e:
+    with  pytest.raises(exc.ResourceClosedError) as e:
         post = metadata().tables['post']
         db.execute((select(post).where(post.c.id == 1)))
     assert 'closed' in str(e.value)
