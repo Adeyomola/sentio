fd84c6cd2b683f8efeb7d2f2b86f5ff8ffd8fd00
Adeyomola <kayzeel@yahoo.com>
2024-01-23 09:14:55 +0100

    updated unit tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,14 +1,19 @@
 import sqlite3
 import pytest
 from verba.db import get_db, init_db, close_db
+from verba.metadata import metadata
 from sqlalchemy import select
-from sqlalchemy.exc import ProgrammingError
+from sqlalchemy.exc import ResourceClosedError
 
 
 def test_get_db(app):
     with app.app_context():
         db = get_db()
         assert db is get_db()
+    with  pytest.raises(ResourceClosedError) as e:
+        post = metadata().tables['post']
+        db.execute((select(post).where(post.c.id == 1)))
+    assert 'closed' in str(e.value)
 
 
 
