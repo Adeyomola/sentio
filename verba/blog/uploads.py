6db5f0c96c5fce85ce7995d8b07c982cacb5c625
Adeyomola <kayzeel@yahoo.com>
2024-01-29 04:27:38 +0100

    still working on image cropping

--- verba/blog/uploads.py
+++ verba/blog/uploads.py
@@ -63,7 +63,7 @@ class Upload:
 
         left = (width - (width * 0.7))/2
         right = (width + (width * 0.4))/2
-        top = (height - (height * 0.7))/2
+        top = (height - (height * 0.5))/2
         down = (height + (height * 0.4))/2
         image = image.crop((left, top, right, down))
         
