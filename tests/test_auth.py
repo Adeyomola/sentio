ad28290fdefb20122af9166a3dde106c7552da46
Adeyomola <kayzeel@yahoo.com>
2024-01-30 03:39:14 +0100

    -

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -25,6 +25,7 @@ def test_login_validation(client, email, password, message):
 
 def test_register(client, app):
     assert client.get('/register').status_code == 200
+    
     result = client.post('/register', data={'username': 'register', 'password': 'register', 'confirm_password': 'register', 'firstname': 'verba', 'lastname': 'registrar', 'email': 'registrar@test.com'})
     assert result.headers['Location'] == '/verify?unverified_email=registrar@test.com'
 
