44fefd6a508a99a6a50c7ab8bc0e0a17dd491566
Adeyomola <kayzeel@yahoo.com>
2024-01-31 20:30:41 +0100

    minor fixes

--- verba/auth/email_auth.py
+++ verba/auth/email_auth.py
@@ -1,23 +1,16 @@
 import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-
 import os
 
 password=os.environ.get('E_PASSWORD')
 address=os.environ.get('ADDRESS')
 smtp_server=os.environ.get('SMTP_SERVER')
 
-msg = MIMEMultipart()
-
 def send_email(email, otp, firstname):
-    msg['Subject'] = 'Welcome to Verba'
-    msg.attach(MIMEText(f'''Hello {firstname},\n\nWelcome to Verba.\nPlease confirm your email address using the OTP: {otp}.\n\nThank you. \n\nDon't Fall Off the Wheel of Words \nVerba. ''', 'plain'))
+    msg = f'''Subject: Welcome to Verba Hello {firstname},\n\nWelcome to Verba.\nPlease confirm your email address using the OTP: {otp}.\n\nThank you. \n\nDon't Fall Off the Wheel of Words \nVerba.'''
     
-
     server = smtplib.SMTP(smtp_server, 587)
     server.starttls()
     server.login(address, password)
-    server.sendmail(address, email, msg.as_string())
+    server.sendmail(address, email, msg)
     server.quit()
         
