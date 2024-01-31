1456edb432bc2f3da97d960522e3166bc8c0b668
Adeyomola <kayzeel@yahoo.com>
2024-01-31 20:32:07 +0100

    minor fixes

--- verba/auth/email_auth.py
+++ verba/auth/email_auth.py
@@ -6,7 +6,7 @@ address=os.environ.get('ADDRESS')
 smtp_server=os.environ.get('SMTP_SERVER')
 
 def send_email(email, otp, firstname):
-    msg = f'''Subject: Welcome to Verba Hello {firstname},\n\nWelcome to Verba.\nPlease confirm your email address using the OTP: {otp}.\n\nThank you. \n\nDon't Fall Off the Wheel of Words \nVerba.'''
+    msg = f'''Subject: Welcome to Verba \n\nHello {firstname},\n\nWelcome to Verba.\nPlease confirm your email address using the OTP: {otp}.\n\nThank you. \n\nDon't Fall Off the Wheel of Words \nVerba.'''
     
     server = smtplib.SMTP(smtp_server, 587)
     server.starttls()
