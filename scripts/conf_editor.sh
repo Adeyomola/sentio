dea0ac74540fdca48d5579791934fa585eb310bd
Adeyomola <kayzeel@yahoo.com>
2024-02-01 03:33:14 +0100

    switching to static totp secret

--- scripts/conf_editor.sh
+++ scripts/conf_editor.sh
@@ -7,4 +7,4 @@ echo "export DB_USER=$DB_USER" >> .env
 echo "export DATABASE=$DATABASE" >> .env
 echo "export HOST=$HOST" >> .env
 echo "export SECRET_KEY=$(python -c 'import secrets; print(secrets.token_hex())')" >> .env
-echo "export TOTP_SECRET=$(python -c 'import secrets; print(base64.b32encode(secrets.token_bytes(5)).decode('utf-8'))" >> .env
+echo "export TOTP_SECRET=$(python -c 'import secrets; import base64; print(base64.b32encode(secrets.token_bytes(5)).decode("utf-8"))')" >> .env
